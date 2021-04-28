b00a2c879bf9b095664a09bf8efc7e7dd042b60b
artiseni <83260595+artiseni@users.noreply.github.com>
2021-04-28 09:27:56 +0700

    Update server.js

--- beckend/server/server.js
+++ beckend/server/server.js
@@ -1,16 +1,13 @@
 const http = require('http')
 const fs = require('fs')
 const qs = require('querystring')
-// const blogs = require('./handler/blogs')
 const jsonData = require('../db/data.json')
-// const controller = require('../handler/controller.js')
-const users = require("../handler/users")
+const users = require("../controller/users")
 
 const port = 3000
 const succ = 200
 
 http.createServer((rq, rs) => {
-    // console.log(jsonData)
     rs.setHeader('Access-Control-Allow-Origin', '*');
 	rs.setHeader('Access-Control-Request-Method', '*');
 	rs.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
@@ -19,18 +16,15 @@ http.createServer((rq, rs) => {
     const isJson = rq.headers['content-type'] == 'application/json'
     const url = rq.url == '' ? '/index' : rq.url
     
-    // console.log(rq.headers['content-type'])
-    // console.log(url)
     
     if (isJson){  
         if (rq.method.toLowerCase() === 'get'){
             switch (url) {
                 case '/blogs':
-                    // res.end(blogs.index(req, res))
+                    
                 break;
                 case '/login':
                     users.userName(rq, rs)
-                    // console.log(`Request type : ${rq.method} | URL ${url}`)
                     break;
                     case '/index':
                     users.userName(rq, rs)
@@ -45,13 +39,12 @@ http.createServer((rq, rs) => {
                 case '/login':
                     users.login(rq, rs)
                     users.change(rq, rs)
-                    // console.log('Ada yang request post nih..')
                 break;
                 case '/signup':
                     users.signup(rq, rs)
                 break;
                 default:
-                    // controller.receivePost(fs, qs, rq, rs)
+
                 break;
             } 
         }
