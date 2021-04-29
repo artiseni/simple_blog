17de7d462854bcbeaff220fac78fdfa760a4417c
artiseni <83260595+artiseni@users.noreply.github.com>
2021-04-30 01:15:01 +0700

    Update server.js

--- beckend/server/server.js
+++ beckend/server/server.js
@@ -8,18 +8,14 @@ const port = 3000
 const succ = 200
 
 http.createServer((rq, rs) => {
-    // console.log(jsonData)
     rs.setHeader('Access-Control-Allow-Origin', '*');
-	rs.setHeader('Access-Control-Request-Method', '*');
-	rs.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
-	rs.setHeader('Access-Control-Allow-Headers', '*');
+    rs.setHeader('Access-Control-Request-Method', '*');
+    rs.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+    rs.setHeader('Access-Control-Allow-Headers', '*');
     rs.setHeader('Content-type', 'application/json')
     const isJson = rq.headers['content-type'] == 'application/json'
     const url = rq.url == '' ? '/index' : rq.url
-    
-    // console.log(rq.headers['content-type'])
-    // console.log(url)
-    
+ 
     if (isJson){  
         if (rq.method.toLowerCase() === 'get'){
             switch (url) {
@@ -28,11 +24,9 @@ http.createServer((rq, rs) => {
                 break;
                 case '/login':
                     users.userName(rq, rs)
-                    // console.log(`Request type : ${rq.method} | URL ${url}`)
                     break;
                     case '/index':
                     users.userName(rq, rs)
-                    // console.log(`Request type : ${rq.method} | URL ${url}`)
                     break;
                 default:
                 
@@ -43,7 +37,6 @@ http.createServer((rq, rs) => {
                 case '/login':
                     users.login(rq, rs)
                     users.change(rq, rs)
-                    // console.log('Ada yang request post nih..')
                 break;
                 case '/signup':
                     users.signup(rq, rs)
@@ -52,7 +45,7 @@ http.createServer((rq, rs) => {
                     users.edit(rq, rs)
                 break;
                 default:
-                    // controller.receivePost(fs, qs, rq, rs)
+
                 break;
             } 
         }
