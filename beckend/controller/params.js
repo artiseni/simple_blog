7875a0723685e1382417f75bc0da7a5c3e5316aa
artiseni <83260595+artiseni@users.noreply.github.com>
2021-05-02 00:54:11 +0700

    Update params.js

--- beckend/controller/params.js
+++ beckend/controller/params.js
@@ -8,16 +8,6 @@ params =  {
                 callback(JSON.parse(data))
             })
         }
-    },
-
-    get(rq, callback=null){
-        let data = ""
-        if (callback){
-            rq.on('data', chunk => data =+ chunk)
-            .on('end', () => {
-                callback(JSON.parse(data))
-            })
-        }
     }
 
 }
